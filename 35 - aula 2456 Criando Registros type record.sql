84b1e03cb05abc98ce2bc73b921deb7e9ede4163
Fabio Macieira <fabiomacieira16@gmail.com>
2025-02-10 12:15:17 -0300

    Refactor record type for stock data and update output formatting

--- 35 - aula 2456 Criando Registros type record.sql
+++ 35 - aula 2456 Criando Registros type record.sql
@@ -1,11 +1,12 @@
 declare
-   type reg_estoque is record (
+   type reg_estoque IS record (
          codigo number(3),
          nome   varchar2(30)
    );
    dados_estoque reg_estoque;
 begin
    dados_estoque.codigo := 10;
-   dados_estoque.nome := 'Produto 1';
-   dmbs_output.put_line('Código: ' || to_char(dados_estoque.codigo) ||  'Nome: ' || to_char(dados_estoque.nome));
+   dados_estoque.nome := 'Fábio Macieira';
+   DBMS_OUTPUT.PUT_LINE('Código: ' || TO_CHAR(dados_estoque.codigo) ||  
+                        ' Nome: ' || dados_estoque.nome);
 end;
